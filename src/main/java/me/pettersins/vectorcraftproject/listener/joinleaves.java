5dbc9a03cf05bf3aa9a31bbb7d172bd44109948e
antasetyawan <78466651+antasetyawan@users.noreply.github.com>
2022-02-08 06:59:30 +0700

    add PlayerQuitEvent add leaves config.yml

--- src/main/java/me/pettersins/vectorcraftproject/listener/joinleaves.java
+++ src/main/java/me/pettersins/vectorcraftproject/listener/joinleaves.java
@@ -6,6 +6,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.Bukkit;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class joinleaves implements Listener {
 
@@ -26,6 +27,13 @@ public class joinleaves implements Listener {
             Bukkit.broadcastMessage(plugin.getConfig().getString("firstjoin").replace("<player>", p.getName()));
 
         }
+
+    }
+    public void onJoin(PlayerQuitEvent c){
+        Player p = c.getPlayer();
+
+            Bukkit.broadcastMessage(plugin.getConfig().getString("leaves").replace("<player>", p.getName()));
+
     }
 
 }
