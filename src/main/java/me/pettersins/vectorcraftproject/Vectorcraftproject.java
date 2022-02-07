9f0b4648cade907653ce971066963332b7f5f0d5
antasetyawan <78466651+antasetyawan@users.noreply.github.com>
2022-02-08 05:49:03 +0700

    addcommands

--- src/main/java/me/pettersins/vectorcraftproject/Vectorcraftproject.java
+++ src/main/java/me/pettersins/vectorcraftproject/Vectorcraftproject.java
@@ -1,13 +1,14 @@
 package me.pettersins.vectorcraftproject;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import me.pettersins.vectorcraftproject.Command.maincommand;
 
 public final class Vectorcraftproject extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        new maincommand(this);
     }
 
     @Override
